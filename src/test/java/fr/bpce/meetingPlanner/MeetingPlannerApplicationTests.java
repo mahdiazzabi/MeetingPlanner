5bce1496fde2f11634f049bae22b0568351d34b0
Mehdi AZZABI <mehdi.azzabi@outlook.com>
2025-03-20 12:01:10 +0100

    init app

---
+++ src/test/java/fr/bpce/meetingPlanner/MeetingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bpce.meetingPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
